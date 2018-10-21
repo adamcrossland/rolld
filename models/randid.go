fac37782eeff865142822375cc47fff0a818fb98
Adam Crossland <adam.crossland@epfrglobal.com>
2018-10-21 17:33:33 -0400

    Resolve #37.
    Changed from reading /dev/urnadom to using rand.Read, which is cross-platform.

--- models/randid.go
+++ models/randid.go
@@ -2,15 +2,13 @@ package models
 
 import (
 	"fmt"
-	"os"
+	"math/rand"
 )
 
 // GetRandomID returns a string representing the requested number of random bytes
 // in Base16. Depends on the existence of /dev/urandom which makes it Linux-specific.
 func GetRandomID(byteCount int) string {
-	f, _ := os.Open("/dev/urandom")
-	defer f.Close()
 	b := make([]byte, byteCount)
-	f.Read(b)
+	rand.Read(b)
 	return fmt.Sprintf("%x", b)
 }
